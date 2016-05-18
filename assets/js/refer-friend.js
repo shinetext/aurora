07dc2df11be8d4fb8eb3ba6d427d918c785aef9a
Jonathan Uy <jonxuy@gmail.com>
2016-05-17 23:38:19 -0400

    Improves referral entry with loading icon and listening for Enter key Users can now hit the Enter key while focus is o, we also now display a loading icon.

--- assets/js/refer-friend.js
+++ assets/js/refer-friend.js
@@ -1,13 +1,16 @@
 /**
  * Handle request to fetch a user's referral info.
  */
-$('#get-referral-info-btn').click(function() {
+var getReferralInfo = function() {
   var phone = $('#get-referral-phone').val();
 
   // @todo phone validation?
   if (phone.length === 0) {
     return;
   }
+
+  // Show loading icon
+  $('#referral-loading-icon').show();
 
   var url = '/referral/' + phone;
   var onSuccess = function(data) {
@@ -22,10 +25,13 @@ $('#get-referral-info-btn').click(function() {
     referralUrl = 'http://www.shinetext.com/?r=' + data.referralCode;
     $('#referral-url').html(referralUrl);
     $('#referral-url').attr('href', referralUrl);
+
+    $('#referral-loading-icon').hide();
   };
   var onError = function(data) {
     $('#referral-info').hide();
     $('#referral-not-found').show(400);
+    $('#referral-loading-icon').hide();
   };
 
   var request = {
@@ -36,4 +42,12 @@ $('#get-referral-info-btn').click(function() {
   };
 
   $.ajax(request);
+};
+
+$('#get-referral-info-btn').click(getReferralInfo);
+$('#get-referral-phone').keypress(function(event) {
+  // User presses Enter key
+  if (event.which === 13) {
+    getReferralInfo();
+  }
 });
