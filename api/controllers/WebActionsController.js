17eef3280853182473697df7d96c1673e4bdbf09
Jonathan Uy <jon@shinetext.com>
2016-08-25 17:23:00 -0400

    Adds a request on signup to add user as a subscriber to MailChimp

--- api/controllers/WebActionsController.js
+++ api/controllers/WebActionsController.js
@@ -27,6 +27,7 @@ module.exports = {
       'person[email]': req.body.email,
       'person[send_gifs]': typeof req.body.send_gifs === 'undefined' ? 'no' : 'yes'
     };
+
     let redirectUrl = '/confirmation?phone=' + req.body.phone + '&firstName='
       + req.body.first_name;
 
@@ -64,8 +65,52 @@ module.exports = {
         return request.postAsync(photonRequest);
       })
       .then(function(response) {
-        // If available, attach the referralCode to the redirect URL
+        let referralCode = '';
         if (response.body && typeof response.body.referralCode === 'string') {
+          referralCode = response.body.referralCode;
+        }
+
+        // Adds as a subscriber to MailChimp if we have an email
+        if (typeof req.body.email === 'string' && req.body.email.length > 0) {
+          // @todo Implement additional email validation?
+          // @todo Not currently handling failed API calls
+          const mailchimpRequest = {
+            method: 'POST',
+            uri: `${sails.config.globals.mailchimpApiUrl}/lists/${sails.config.globals.mailchimpListId}/members`,
+            json: true,
+            auth: {
+              user: sails.config.globals.mailchimpApiAuthUser,
+              pass: sails.config.globals.mailchimpApiAuthPass,
+            },
+            body: {
+              email_address: req.body.email,
+              status: 'subscribed',
+              merge_fields: {
+                FNAME: req.body.first_name,
+                PHONE: req.body.phone,
+                REFERRAL: referralCode,
+              },
+            },
+          };
+
+          request.postAsync(mailchimpRequest)
+            .then((response) => {
+              if (! response || ! response.body) {
+                sails.log.error('Invalid response received from MailChimp subscribe call.');
+              }
+              else {
+                sails.log.info('Successful MailChimp subscribe');
+                sails.log.info(`  id: ${response.body.id}`);
+                sails.log.info(`  unique_email_id: ${response.body.unique_email_id}`);
+              }
+            })
+            .catch((err) => {
+              sails.log.error(err);
+            });
+        }
+
+        // If available, attach the referralCode to the redirect URL
+        if (referralCode.length > 0) {
           redirectUrl += '&referralCode=' + response.body.referralCode;
         }
 
