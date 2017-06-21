29c11be780bb9881bab22225ede68814dc21ec8b
Catherine Lau <cat@shinetext.com>
2017-06-21 14:05:07 -0400

    Add sails-hook-babel config file with babel-polyfill included to support generator transforms when using babel
    Also modifies 500 response to string type when calling res.redirect(500) as potential fix for url.indexOf error.

---
+++ config/babel.js
@@ -0,0 +1,11 @@
+/**
+ * Blueprint API Configuration
+ * (sails.config.babel)
+ *
+ * Custom config for SailsJS hook to activate ES6/7 Javascript code for your whole sails app
+ *
+ */
+
+module.exports.babel = {
+  polyfill: true
+};
