b4afed55f671dfa8879e0857b37647f75d1a6424
Jonathan Uy <jon@shinetext.com>
2017-08-03 01:00:25 -0400

    Fixes friends data sent from BetaSignUpForm to what's submitted in the join request
    Also starts adjusting styling on the beta fields to display side by side.

--- views/components/BetaSignUpForm.js
+++ views/components/BetaSignUpForm.js
@@ -4,18 +4,37 @@ import FormField from './FormField';
 const BetaSignUpForm = props => {
   return (
     <div className="BetaSignUpForm">
-      <fieldset>
-        <FormField isRequired label="Name #1" type="text" fieldName="beta_name_1" value="" />
-        <FormField isRequired label="Phone #1" type="tel" fieldName="beta_phone_1" value="" />
-      </fieldset>
-      <fieldset>
-        <FormField isRequired label="Name #2" type="text" fieldName="beta_name_2" value="" />
-        <FormField isRequired label="Phone #2" type="tel" fieldName="beta_phone_2" value="" />
-      </fieldset>
-      <fieldset>
-        <FormField isRequired label="Name #3" type="text" fieldName="beta_name_3" value="" />
-        <FormField isRequired label="Phone #3" type="tel" fieldName="beta_phone_3" value="" />
-      </fieldset>
+      <label>Friend invites:</label>
+      <div className="friend-fields-container">
+        <FormField
+          isRequired
+          label="Name #1"
+          type="text"
+          fieldName="friends[0][first_name]"
+          value=""
+        />
+        <FormField isRequired label="Phone #1" type="tel" fieldName="friends[0][phone]" value="" />
+      </div>
+      <div className="friend-fields-container">
+        <FormField
+          isRequired
+          label="Name #2"
+          type="text"
+          fieldName="friends[1][first_name]"
+          value=""
+        />
+        <FormField isRequired label="Phone #2" type="tel" fieldName="friends[1][phone]" value="" />
+      </div>
+      <div className="friend-fields-container">
+        <FormField
+          isRequired
+          label="Name #3"
+          type="text"
+          fieldName="friends[2][first_name]"
+          value=""
+        />
+        <FormField isRequired label="Phone #3" type="tel" fieldName="friends[2][phone]" value="" />
+      </div>
       <FormField type="hidden" fieldName="friends_opt_in_path" value={props.optin} />
     </div>
   );
