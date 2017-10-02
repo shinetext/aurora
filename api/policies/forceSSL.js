52da30cb0e63acf3cce41925e4da5f4417450656
Jonathan Uy <jon@shinetext.com>
2017-10-02 11:38:08 -0400

    Adds a DISABLE_FORCE_SSL env var to disable the forceSSL policy
    Making it an environment variable also makes it easy to turn off this feature should things go wrong.

--- api/policies/forceSSL.js
+++ api/policies/forceSSL.js
@@ -4,8 +4,13 @@
  * Redirect insecure connections to https.
  */
 module.exports = (req, res, next) => {
+  // Don't attempt to redirect connections to https if:
+  //   - This is a production environment
+  //   - Tee DISABLE_FORCE_SSL environment variable is set to "true"
+  //   - The connection is already deemed secure
   if (
     process.env.NODE_ENV !== 'production' ||
+    process.env.DISABLE_FORCE_SSL === 'true' ||
     ConnectionService.isSecure(req)
   ) {
     next();
