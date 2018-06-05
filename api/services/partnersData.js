b9e61452deb91568f9bec763b3bd09892d7b912a
kaladin9017 <sanchochris@gmail.com>
2018-06-05 19:07:40 -0400

    Crop photo and change copy

--- api/services/partnersData.js
+++ api/services/partnersData.js
@@ -124,9 +124,9 @@ const partnersData = {
     },
   },
   wybmn: {
-    name: 'Get texts inspired by Mr. Rogers',
-    imageUrl: '/images/partners/mr-rogers-2.jpg',
-    copy: '<p>Won\'t You Be My Neighbor in Select Theaters June 8</p>',
+    name: 'Get texts from Shine inspired by Mr. Rogers',
+    imageUrl: '/images/partners/mr-rogers-3.jpg',
+    copy: '<p>"Won\'t You Be My Neighbor" in Select Theaters June 8</p>',
     confirmation: {
       imageUrl: '/images/confirmation-header-happy-dance.gif',
       copy: "You're all signed up!",
